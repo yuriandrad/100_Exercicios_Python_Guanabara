230540304056f7176223d5a04957cc6f460ec36c
Yuri <andradeyuri010@gmail.com>
2022-05-12 23:10:49 -0300

    Tratando dados e fazendo contas

---
+++ ex007 - Média Aritmética.py
@@ -0,0 +1,5 @@
+n=float(input('Digite sua nota: '))
+nn=float(input('Agora digite a segunda nota: '))
+m=(n+nn)/2
+print('A média foi de {:.2f}'.format(m))
+
---
+++ ex014 - Conversor de Temperaturas.py
@@ -0,0 +1,3 @@
+cel=float(input('Digite a temperatura em °C:'))
+fah= cel * 1.8 + 32
+print('A temperatura {:.1f}°C, em Fahrenheit fica {:.1f}°F'.format(cel, fah))
